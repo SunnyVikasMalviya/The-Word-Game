b275170a487c555a684062dbc74625bb2a7cbe07
Vikas Malviya <40855821+SunnyVikasMalviya@users.noreply.github.com>
2018-09-24 21:55:59 +0530

    Update The_Word_Game.py

--- The_Word_Game.py
+++ The_Word_Game.py
@@ -10,7 +10,13 @@ if __name__ == '__main__':
     #Given letters can be used as many times as required by the player 
     #Points will be given by summing the scores of each letter
 
-    from random import randint
+    import random
+    
+    #import os
+    #clear = lambda : os.system('cls')
+    #The above two lines are not working; wanted to clear the screen
+    """Many Commented statements were used for debugging and are not \
+    removed in order to ease future modifications"""
     
     
     #Dictionary listing the points of all the letters 
@@ -18,7 +24,9 @@ if __name__ == '__main__':
     		 "h": 4, "k": 5, "j": 8, "m": 3, "l": 1, "o": 1, "n": 1, "q": 10, \
     		 "p": 3, "s": 1, "r": 1, "u": 1, "t": 1, "w": 4, "v": 4, "y": 4,  \
     		 "x": 8, "z": 10}
-    
+    consonents = ['B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P',\
+                  'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z']
+    vowels = ['A', 'E', 'I', 'O', 'U']
     players_score = {}
     		 
     #Function to calculate the score of a player for a word that just got entered
@@ -26,15 +34,24 @@ if __name__ == '__main__':
         total = 0
         for ch in word:
             total += score[ch]
-        print(total)
+        #The escape sequence \033[36m is used for coloring text into darkcyan
+        print("\033[36m",total,"\033[0m")
         return total
     
     
     #Function to display random letters
     def display_random_letters():
         random_letter_produced = []
+        num_vowels = random.randint(1,5)
+        #print(num_vowels)
+        #This first loop generates 5 to 9 random consonents
+        while len(random_letter_produced) < (10 - num_vowels)  :
+            random_letter = random.choice(consonents)
+            if random_letter not in random_letter_produced :
+                random_letter_produced.append(random_letter)
+        #This second loop generates 5 to 1 random vowels that are left
         while len(random_letter_produced) < 10 :
-            random_letter = chr(randint(65,90))
+            random_letter = random.choice(vowels)
             if random_letter not in random_letter_produced :
                 random_letter_produced.append(random_letter)
                 #remove the pound to print the letters in random order as they get
@@ -66,7 +83,7 @@ if __name__ == '__main__':
         for first_letter in entered_word:
             break
         first_letter.lower()
-        file_name = first_letter + ".txt"
+        file_name = "Letters\\" + first_letter + ".txt"
         #check whether word is meaning-ful or not
         ft = open(file_name, "r")
         #file_words contains all words in the first_letter file
@@ -123,13 +140,13 @@ if __name__ == '__main__':
     #Function to display the score of each player and the winner
     def display_results(number_of_players):
         for x in range(0, number_of_players):
-            print("Player %d : %d") % (x+1, players_score[x])
+            print("Player ", x+1, ":", players_score[x])
         v = list(players_score.values())
         k = list(players_score.keys())
         winner =k[v.index(max(v))] 
-        print("One last thing. In case of more than one players having the \
-              highest score, only one lucky player gets to win.")
-        print("And the winner is ...(Suspence)... Player %d") % (winner+1)
+        print("One last thing. In case of more than one players having the" \
+              "highest score, only one lucky player gets to win.")
+        print("And the winner is ...(Suspence)... Player %d" % (winner+1))
         out()
             
             
@@ -139,16 +156,23 @@ if __name__ == '__main__':
         print ("1. Play")
         print ("2. Read Rules")
         print ("3. Exit")
-        intro_choice = int(input("Choose 1, 2 or 3:"))
-        if intro_choice == 1:
-            play()
-        elif intro_choice == 2:
-            rules()
-        elif intro_choice == 3:
-            out()
-        else :
-            print("\nGarib hai kya? Jitna bola utna ker na. Choose either 1, 2 or 3")
+        try:
+            intro_choice = int(input("Choose 1, 2 or 3:"))
+            if intro_choice == 1:
+                play()
+            elif intro_choice == 2:
+                rules()
+            elif intro_choice == 3:
+                out()
+            else :
+                print("\nGarib hai kya? Jitna bola utna ker na. Choose either 1, 2 or 3")
+                intro()
+        except ValueError:
+            print("\nEnter an Intergeral Value.")
+            #clear()       #To Clear Screen but it is not working
             intro()
+        except :
+            print("Something went very wrong.")
         
     
     #Function to play the game
@@ -162,9 +186,9 @@ if __name__ == '__main__':
         #able to hold scores of all the players
         for x in range(0, num_of_players):
             players_score[x] = 0  
-        print("Lets start then")
-        for x in range(1, 6):
-            print("Round ",str(x))
+        print("\nLets start then")
+        for x in range(1, 4):
+            print("\033[4mRound ",str(x),"\033[0m")
             for y in range(0, num_of_players):            
                 print("Player %d to play" % (y+1))
                 random_letter_produced = display_random_letters()
@@ -172,12 +196,18 @@ if __name__ == '__main__':
                 #random_letter_produced.append('n')
                 entered_word = validate_entered_word(random_letter_produced)
                 players_score[y] += scoring(entered_word)
+                print(players_score[y])
+                #players_score
         display_results(num_of_players)
         
     
     #Function to display the rules
     def rules():
-        print("Simple rules\n")
+        #clear()
+        #The escape sequence \033[4m Text is used to underline Text
+        #The escape sequence \033[0m is used to represent where to end the \
+        #previously used escape sequence... in this case underline
+        print("\n\n\033[4mSimple rules\033[0m\n")
         print("Maximum 5 and minimum 2 players can play the game at a time")
         print("5 rounds will be played")
         print("Players will be given 10 random letters in alphabetical order", \
@@ -186,15 +216,16 @@ if __name__ == '__main__':
         print("Words can be anything between 3 to 20 letters")
         print("Given letters can be used as many times as required by the player")
         print("Points will be given by summing the scores of each letter")
-        print("Zero points will be awarded for making words without meaning or \
-              with size outside the size limits\n")
+        print("Zero points will be awarded for making words without meaning" \
+              "with size outside the size limits\n")
         intro()
         
     
     #Function to come out of the game
     def out():
         print("Bye then! Go and have a life.")
-       
+    
+    
     #scoring()
     #display_random_letters()
     #validate_entered_word()
